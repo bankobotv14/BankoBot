93675830e80c27ace07abce0002b7a38e0f7db18
Michael Rittmeister <michael@rittmeister.in>
2021-01-19 11:45:12 +0100

    Add info command (#3)
    * Add info command
    
    * Added ci workflow
    
    * (Hopefully) fixed ci
    
    * Finally fixed ci
    
    * Made gradlew executable
    
    * Apply requested changes
    - Listen to mentions
    
    Co-authored-by: NyCodeGHG <nycode@protonmail.com>

---
+++ src/main/kotlin/de/nycode/bankobot/utils/GitHubUtil.kt
@@ -0,0 +1,82 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 BankoBot Contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package de.nycode.bankobot.utils
+
+import de.nycode.bankobot.BankoBot
+import io.ktor.client.request.*
+import io.ktor.http.*
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+object GitHubUtil {
+    suspend fun retrieveContributors(): List<GithubContributor> {
+        return BankoBot.httpClient.get(API_BASE) {
+            url {
+                path(GET_CONTRIBUTORS_ENDPOINT)
+            }
+        }
+    }
+
+    private val API_BASE = Url("https://api.github.com")
+    private val GET_CONTRIBUTORS_ENDPOINT = "repos/nycodeghg/bankobot/contributors"
+}
+
+@Serializable
+class GithubContributor(
+    val login: String,
+    val id: Long,
+    @SerialName("node_id")
+    val nodeId: String,
+    @SerialName("avatar_url")
+    val avatarUrl: String,
+    @SerialName("gravatar_id")
+    val gravatarId: String,
+    val url: String,
+    @SerialName("html_url")
+    val htmlUrl: String,
+    @SerialName("followers_url")
+    val followersUrl: String,
+    @SerialName("following_url")
+    val followingUrl: String,
+    @SerialName("gists_url")
+    val gistsUrl: String,
+    @SerialName("starred_url")
+    val starredUrl: String,
+    @SerialName("subscriptions_url")
+    val subscriptionsUrl: String,
+    @SerialName("organizations_url")
+    val organizationsUrl: String,
+    @SerialName("repos_url")
+    val reposUrl: String,
+    @SerialName("events_url")
+    val eventsUrl: String,
+    @SerialName("received_events_url")
+    val received_eventsUrl: String,
+    val type: String,
+    @SerialName("site_admin")
+    val siteAdmin: Boolean,
+    val contributions: Int,
+)
